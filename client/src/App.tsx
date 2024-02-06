ae680fdbf5cae9bd715888533f8ec3d49190ef69
CGimenez <carlosgibe@gmail.com>
2024-02-06 18:31:28 +0100

    refactorized

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,34 @@
-import { useEffect, useState } from 'react';
-import { io, Socket } from 'socket.io-client';
+import { useState } from 'react';
+import ChatRoom from './ChatRoom';
 
-import './App.css';
+function Home() {
+  const [userName, setUserName] = useState('');
+  const [accepted, setAccepted] = useState(false);
 
-function App() {
-  const [socket, setSocket] = useState<Socket | null>(null);
-  const [messages, setMessages] = useState<string[]>([]);
-  const [mensajeNuevo, setMensajeNuevo] = useState<string>('');
-
-  useEffect(() => {
-    const socket = io('http://localhost:3000');
-    setSocket(socket);
-
-    socket.on('bienvenida', (message) => {
-      console.log(message);
-    });
-
-    socket.on('mensaje', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  const enviarMensaje = (newMessage: string) => {
-    if (socket && newMessage.trim() !== '') {
-      socket.emit('mensaje', newMessage);
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
-      setMensajeNuevo('');
+  const handleAccept = () => {
+    if (userName.trim() !== '') {
+      setAccepted(true);
     }
   };
 
   return (
-    <>
-      <div>
-        <h1>Chat</h1>
-        <div className="div-chat">
-          {messages.map((mensaje, index) => (
-            <p key={index}>{mensaje}</p>
-          ))}
+    <div>
+      {!accepted ? (
+        <div>
+          <h1>Bienvenido al Chat</h1>
+          <input
+            type="text"
+            placeholder="Ingresa tu nombre"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button onClick={handleAccept}>Aceptar</button>
         </div>
-        <input
-          type="text"
-          placeholder="Escribe tu mensaje"
-          value={mensajeNuevo}
-          onChange={(e) => setMensajeNuevo(e.target.value)}
-        />
-        <button onClick={() => enviarMensaje(mensajeNuevo)}>Enviar</button>
-      </div>
-    </>
+      ) : (
+        <ChatRoom userName={userName} />
+      )}
+    </div>
   );
 }
 
-export default App;
+export default Home;

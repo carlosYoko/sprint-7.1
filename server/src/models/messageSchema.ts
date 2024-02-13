9d7d0a7db92d87b4fb34d03f8c4172671ead7cbe
CGimenez <carlosgibe@gmail.com>
2024-02-13 17:25:40 +0100

    install dependencies and create messageSchema

---
+++ server/src/models/messageSchema.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  userName: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;

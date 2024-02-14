d0b00c17e735c80cdf19f3106666fbb4d742cf97
CGimenez <carlosgibe@gmail.com>
2024-02-14 17:29:17 +0100

    refactorized

--- client/src/components/GoogleLoginButton.tsx
+++ client/src/components/GoogleLoginButton.tsx
@@ -2,24 +2,28 @@ import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 
+type TDecodedToken = {
+  name: string;
+};
+
 function GoogleLoginButton() {
-  type TDecodedToken = {
-    name: string;
-  };
   const navigate = useNavigate();
 
   return (
     <div className="google-login-button">
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          const decoded: TDecodedToken = jwtDecode(
-            credentialResponse?.credential
-          );
-          console.log(decoded.name);
-          navigate('/rooms-form', { state: { userName: decoded.name } });
+          const credential = credentialResponse?.credential;
+          if (credential) {
+            const decoded: TDecodedToken = jwtDecode(credential);
+            console.log(decoded.name);
+            navigate('/rooms-form', { state: { userName: decoded.name } });
+          } else {
+            console.log('Error al hacer login');
+          }
         }}
         onError={() => {
-          console.log('Login failed');
+          console.log('Error al hacer login');
         }}
       />
     </div>
